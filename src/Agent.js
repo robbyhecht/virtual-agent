8639572a39af9b7296e6c03ec06f1d7200fac9b8
robbyhecht <robbyhecht@gmail.com>
2018-12-05 11:50:49 -0600

    updated index and agent files

--- src/Agent.js
+++ src/Agent.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Agent.css';
+import ApplicationViews from "./components/ApplicationViews"
+import Navbar from "./components/navbar/Navbar"
 import {  } from 'reactstrap'
 
 class Agent extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <ApplicationViews />
+    </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Agent from './Agent';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Agent from "./Agent";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<Agent />, document.getElementById('root'));
+ReactDOM.render(<Agent />, document.getElementById("root"));
