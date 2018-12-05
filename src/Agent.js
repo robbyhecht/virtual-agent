a8f4bab7d2926ae182ed2d611cfd78e0cad26918
robbyhecht <robbyhecht@gmail.com>
2018-12-05 11:17:05 -0600

    initial setup files/folders for project

---
+++ src/Agent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Agent.css';
+import {  } from 'reactstrap'
+
+class Agent extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default Agent;
