70516a9855287eb665ad893739992ac577850c61
robbyhecht <robbyhecht@gmail.com>
2018-12-05 12:36:42 -0600

    fixes to index, app, agent and basic navbar added

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ApplicationViews extends Component {
+  render() {
+    return(
+      <React.Fragment>
+        put stuff here
+      </React.Fragment>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom"
 import "./index.css";
 import Agent from "./Agent";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<Agent />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Agent/>
+  </Router>, document.getElementById("root"));
