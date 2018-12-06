0f52e6eb468b55a59896a41a107ef2222ab9a597
robbyhecht <robbyhecht@gmail.com>
2018-12-06 10:33:03 -0600

    first draft of VenuesCard.js complete

---
+++ src/components/venues/VenuesCard.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import { Card, Button, CardHeader, CardFooter, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Link } from "react-router-dom";
+import "./Venues.css"
+export default class venueCard extends Component {
+
+  render() {
+    return(
+      <div>
+      <Card key={this.props.venue.id} className="venueCard">
+        <CardHeader tag="h3">{this.props.venue.name}</CardHeader>
+        <CardBody>
+          <CardTitle>{this.props.venue.city}, {this.props.venue.state}</CardTitle>
+          <CardText>{this.props.venue.buyer}</CardText>
+          <CardText>{this.props.venue.email}</CardText>
+          <CardText>{this.props.venue.phone}</CardText>
+          <CardText>{this.props.venue.url}</CardText>
+          <CardText>{this.props.venue.notes}</CardText>
+          <Button>Go somewhere</Button>
+        </CardBody>
+        <CardFooter className="text-muted">          
+          <CardText>Have played: {this.props.venue.played}</CardText>
+          <CardText>Favorite: {this.props.venue.favorites}</CardText>
+        </CardFooter>
+      </Card>
+    </div>
+    )
+  }
+}
